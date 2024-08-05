548780ecc71a0b69eb326c6ee60b16606c4a1dc2
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-08-05 13:35:09 -0300

    update responsividade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // OUTDOOR 
 var copy = document.querySelector(".logos-slide").cloneNode(true);
       document.querySelector(".logos").appendChild(copy);
+
+
+// MOBILE MENU
+function toggleMenu() {
+      const navList = document.querySelector('.nav-list');
+      navList.classList.toggle('active');
+  }
