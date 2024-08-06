46f1c4df6ef2d7b13371e80031205e5de093d1ad
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-08-06 09:45:27 -0300

    update mobile menu

--- js/script.js
+++ js/script.js
@@ -23,25 +23,3 @@ function sendEmail() {
       var body = "Olá,\n\nGostaria de entrar em contato a respeito dos seus serviços.\n\nMeu email é: ";
       window.location.href = "mailto:blueprints@outlook.com.br?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
   });
-
-// PREÇOS
-document.getElementById('btnHoras').addEventListener('click', function() {
-    document.querySelectorAll('.plano').forEach(function(el) {
-        el.classList.remove('active');
-    });
-    document.querySelectorAll('.plano-horas').forEach(function(el) {
-        el.classList.add('active');
-    });
-});
-
-document.getElementById('btnProjetos').addEventListener('click', function() {
-    document.querySelectorAll('.plano').forEach(function(el) {
-        el.classList.remove('active');
-    });
-    document.querySelectorAll('.plano-projetos').forEach(function(el) {
-        el.classList.add('active');
-    });
-});
-
-// Exibir inicialmente os planos por horas
-document.getElementById('btnHoras').click();
